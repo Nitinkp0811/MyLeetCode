ce1e5f82e26cdb4220cc1e7437f4188da039aaba
Nitin Kumar Patel <nitin8112006@gmail.com>
2026-03-30 17:46:12 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (79.18%) - LeetSync

---
+++ 342-power-of-four/power-of-four.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool isPowerOfFour(int n) {
+        if((n > 0) && (n & (n - 1)) == 0) {
+            if((n - 1) % 3 == 0) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+};
