7c1831bd43715a675d31cfc3c508be511441b318
Nitin Kumar Patel <nitin8112006@gmail.com>
2026-03-31 18:02:21 +0530

    Time: 0 ms (100.00%) | Memory: 17.5 MB (53.65%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,6 +1,5 @@
 class Solution {
 public:
-
     int firstPosition(vector<int>& nums, int target) {
         int start = 0;
         int end = nums.size() - 1;
@@ -14,10 +13,10 @@ public:
                 ans = mid;
                 end = mid - 1;
             }
-            else if(target < nums[mid]) {
-                end = mid - 1;
-            } else {
+            else if(nums[mid] < target) {
                 start = mid + 1;
+            } else {
+                end = mid - 1;
             }
         }
 
@@ -27,25 +26,26 @@ public:
     int lastPosition(vector<int>& nums, int target) {
         int start = 0;
         int end = nums.size() - 1;
-        
+
         int ans = -1;
 
         while(start <= end) {
             int mid = start + (end - start) / 2;
 
-            if(target == nums[mid]) {
+            if(nums[mid] == target) {
                 ans = mid;
                 start = mid + 1;
             }
-            else if(target < nums[mid]) {
-                end = mid - 1;
+            else if(nums[mid] < target) {
+                start = mid + 1;
             } else {
-                start = mid +1;
+                end = mid - 1;
             }
         }
 
         return ans;
     }
+
     vector<int> searchRange(vector<int>& nums, int target) {
         vector<int> ans;
 
