3d7864c9546533b4ab73402461acb45b0ca892c1
Nitin Kumar Patel <nitin8112006@gmail.com>
2026-04-01 18:06:24 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (42.69%) - LeetSync

--- 326-power-of-three/power-of-three.cpp
+++ 326-power-of-three/power-of-three.cpp
@@ -1,17 +1,16 @@
 class Solution {
 public:
     bool isPowerOfThree(int n) {
-        if(n <= 0) {
-            return false;
-        }
+        if(n <= 0) return false;
 
         while(n % 3 == 0) {
             n /= 3;
-            // if(n == 1) {
-            //     return true;
-            // }
         }
 
-        return n == 1;
+        if(n == 1) {
+            return true;
+        }
+
+        return false;
     }
 };
